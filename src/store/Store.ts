97567cf694e115a5ca77a559081099b86ddacf97
anton62k <anton62k@gmail.com>
2019-07-24 23:06:47 +0300

    refactoring Store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,15 +2,19 @@ import { action, computed, observable } from "mobx";
 import { IStore, CharStat } from "./IStore";
 
 export class Store implements IStore {
-  @observable chars: CharStat[] = [];
+  @computed get chars(): CharStat[] {
+    const chars: CharStat[] = [...this.map.entries()].map(([key, value]) => ({
+      char: key,
+      count: value
+    }));
+    chars.sort(this.sortCharStat);
+    return chars;
+  }
 
   @action
   setValue(value: string): void {
-    const mapper: CharStatMapper = this.getCharStatMapper(value);
-    const chars: CharStat[] = this.getCharStat(mapper);
-    chars.sort(this.sortCharStat);
-
-    this.chars = chars;
+    const map: Map<string, number> = this.getCharStatMapper(value);
+    this.updateMap(map);
   }
 
   @action
@@ -23,29 +27,30 @@ export class Store implements IStore {
     return Object.keys(this._allowedChars).sort();
   }
 
-  private getCharStat(mapper: CharStatMapper): CharStat[] {
-    return Object.entries(mapper).map(([key, value]) => ({
-      char: key,
-      count: value
-    }));
-  }
-
-  private getCharStatMapper(value: string): CharStatMapper {
-    const mapper: { [field: string]: number } = {};
+  private getCharStatMapper(value: string): Map<string, number> {
+    const mapper: Map<string, number> = new Map<string, number>();
 
     const chars: string[] = value.split("");
 
     chars.forEach(char => {
       if (this._allowedChars[char]) {
-        if (!mapper[char]) {
-          mapper[char] = 0;
-        }
-
-        mapper[char]++;
+        mapper.set(char, (mapper.get(char) || 0) + 1);
       }
     });
 
     return mapper;
+  }
+
+  private updateMap(newMap: Map<string, number>): void {
+    this.map.forEach((value, key) => {
+      if (!newMap.has(key)) {
+        this.map.delete(key);
+      }
+    });
+
+    newMap.forEach((value, key) => {
+      this.map.set(key, value);
+    });
   }
 
   private getUniqChars(value: string): AllowedChars {
@@ -61,9 +66,9 @@ export class Store implements IStore {
     return a.char.charCodeAt(0) - b.char.charCodeAt(0);
   }
 
+  @observable private map: Map<string, number> = new Map<string, number>();
+
   private _allowedChars: AllowedChars = {};
 }
 
-export type AllowedChars = { [key: string]: true };
-
-export type CharStatMapper = { [field: string]: number };
+type AllowedChars = { [key: string]: true };
