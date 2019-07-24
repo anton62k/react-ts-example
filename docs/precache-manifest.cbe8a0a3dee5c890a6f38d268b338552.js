facadb0cd4d4441c92729dd673d40656fbcbb16a
anton62k <anton62k@gmail.com>
2019-07-24 19:08:15 +0300

    added build

---
+++ docs/precache-manifest.cbe8a0a3dee5c890a6f38d268b338552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d292091752afa0bc42714f17ea77b4d",
+    "url": "/react-ts-example/index.html"
+  },
+  {
+    "revision": "3c2d700c9723d1f20b64",
+    "url": "/react-ts-example/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "3016707c6cf979fda22a",
+    "url": "/react-ts-example/static/css/main.34f518db.chunk.css"
+  },
+  {
+    "revision": "3c2d700c9723d1f20b64",
+    "url": "/react-ts-example/static/js/2.cc94b045.chunk.js"
+  },
+  {
+    "revision": "3016707c6cf979fda22a",
+    "url": "/react-ts-example/static/js/main.404263da.chunk.js"
+  },
+  {
+    "revision": "5c274e892177215ebf14",
+    "url": "/react-ts-example/static/js/runtime~main.eaa30a66.js"
+  }
+]);
